9f75e2044b7d8fd4bd712b09a14b390dd4381647
kaleikautakaoka <107520051+kaleikautakaoka@users.noreply.github.com>
2023-04-15 12:26:05 -0600

    setting vars and api

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Psychic-Weather-App
+
 ## Acceptance Criteria
+
+https://kaleikautakaoka.github.io/Psychic-Weather-App/
 
 ```
 GIVEN a weather dashboard with form inputs
@@ -18,3 +21,4 @@ Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrie
 **Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
 
 You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+```
--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,31 @@
-'strict'
+"strict";
+
+$("#predictBtn").on("click", predictBtn);
+
+function predictBtn(e) {
+  const city = $("#inputCity").val();
+  const state = $("#inputState").val();
+  const country = $("#inputCountry").val();
+  const limit = 10;
+  //Place swirl on the screen
+
+  $.ajax({
+    url: `http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={limit}&appid={API key}`,
+  }).done((coords) => {
+    const parsedCoords = JSON.parse(coords);
+    const lad = parsedCoords["lad"];
+    const lon = parsedCoords["lon"];
+
+    $.ajax({
+      url: `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`,
+    }).done((forcast) => {
+      const parsedForcast = JSON.parse(forcast);
+      const innerContainer = $("<div>");
+
+      innerContainer.text = parsedForcast["forcast"];
+      $("#result_container").append(innerContainer);
+    });
+  });
+}
+// add  
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
