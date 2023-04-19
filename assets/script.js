bc4f5ac2b6849a45cceb7562a4626620aa595f4e
kaleikautakaoka <107520051+kaleikautakaoka@users.noreply.github.com>
2023-04-18 23:05:49 -0600

    html update for markup

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,37 @@
 "strict";
-
-$("#predictBtn").on("click", predictBtn);
-
+$(document).ready(function () {
+  $("#predictBtn").on("click", predictBtn);
+})
+  
+//Function vars for city, state, country, and set limit for api calls
 function predictBtn(e) {
+  e.preventDefault();
   const city = $("#inputCity").val();
-  const state = $("#inputState").val();
-  const country = $("#inputCountry").val();
-  const limit = 10;
+  // const state = $("#inputState").val();
+  // const country = $("#inputCountry").val();
+  
   //Place swirl on the screen
-
-  $.ajax({
-    url: `http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={limit}&appid={API key}`,
-  }).done((coords) => {
-    const parsedCoords = JSON.parse(coords);
-    const lad = parsedCoords["lad"];
-    const lon = parsedCoords["lon"];
-
-    $.ajax({
-      url: `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`,
-    }).done((forcast) => {
-      const parsedForcast = JSON.parse(forcast);
-      const innerContainer = $("<div>");
-
-      innerContainer.text = parsedForcast["forcast"];
+  //api ajax call for coordinates for the location
+  fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=31ece337564bb8d05621f9f35ad27950',
+  ).then(function (coords) {
+    return coords.json();
+  }).then(function (parsedCoords) {
+    console.log(parsedCoords[0].lat);
+    const lat = parsedCoords[0].lat;
+    const lon = parsedCoords[0].lon;
+    getWeather(city);
+  });
+};
+  //Another api call to get forecast
+  function getWeather(city){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=31ece337564bb8d05621f9f35ad27950`,
+    ).then(function(forcast) {
+      return forcast.json();
+    }).then(function (parsedForcast) {
+      innerContainer = $("");
+      console.log(parsedForcast);
+      //Result to be placed in result container
+      innerContainer.text = parsedForcast;
       $("#result_container").append(innerContainer);
     });
-  });
-}
-// add  
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+  };
